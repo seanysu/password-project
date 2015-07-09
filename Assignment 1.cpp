1b37cb4c7d336a803dc1cce8cff12df8c7ce6d86
sean su <goseansu@gmail.com>
2015-07-08 21:59:47 -0700

    The original commit for the password project

---
+++ Assignment 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+   cout << "Hello World\n";
+   cout << "My family name is Su" << endl;
+   cout << "My student ID is 11076571" << endl << endl;
+   cout << "I noticed in the syllabus that students need to post in the forum"
+      << " weekly or they may get dropped!" << endl << endl;
+   return 0;
+}
+
+/* ---------------------- paste of run ---------------------
+
+Hello World
+My family name is Su
+My student ID is 11076571
+
+I noticed in the syllabus that students need to post in the forum weekly or they
+may get dropped!
+
+Press any key to continue . . .
+
+--------------------------------------------------------- */
